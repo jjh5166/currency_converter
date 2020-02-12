88d4fa0dc2e83d2b55d95aec543459c4333d761a
John <jjh5166@gmail.com>
2020-02-11 19:30:48 -0500

    Inputs on Home screen

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextUnput'
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Logo />
-  </Container>
-);
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+class Home extends Component {
+  handlePressBaseCurreny = () => {
+    console.log('press base');
+  }
+  handlePressQuoteCurreny = () => {
+    console.log('press quote');
+  }
+  handleTextChange = (text) => {
+    console.log('texy change ', text);
+
+  }
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurreny}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType='numeric'
+          onChangeText={this.handleTextChange}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurreny}
+          editable={false}
+          value={TEMP_QUOTE_PRICE}
+        />
+      </Container>
+    );
+  }
+}
+
+export default Home;
