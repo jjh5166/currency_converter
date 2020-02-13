45342ce594d02ca9b86786731911e172f18e83ab
John <jjh5166@gmail.com>
2020-02-12 19:58:15 -0500

    Header with gear icon

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -6,6 +6,7 @@ import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput'
 import { ClearButton } from '../components/Buttons'
 import { LastConverted } from '../components/Text'
+import { Header } from '../components/Header'
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
@@ -27,9 +28,13 @@ class Home extends Component {
   handleSwapCurrency = () => {
     console.log('press swap')
   }
+  handleOptionsPress = () => {
+    console.log('press options')
+  }
   render() {
     return (
       <Container>
+        <Header onPress={this.handleOptionsPress} />
         <StatusBar translucent={false} barStyle="light-content" />
         <Logo />
         <InputWithButton
