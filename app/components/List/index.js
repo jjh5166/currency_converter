9b8cccb22c1f0a83663e3fc0235576012db679cf
John <jjh5166@gmail.com>
2020-02-13 00:10:18 -0500

    List

---
+++ app/components/List/index.js
@@ -0,0 +1,5 @@
+import ListItem from './ListItem';
+import Icon from './Icon';
+import Separator from './Separator';
+
+export { ListItem, Icon, Separator };
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Text, FlatList, View, StatusBar } from 'react-native';
+import { Text, FlatList, View, StatusBar, ColorPropType } from 'react-native';
 
+import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
+const TEMP_CURRENT_CURRENCY = 'CAD';
 
-const CurrencyList = () => (
-  <View style={{ flex: 1 }}>
-    <StatusBar barStyle="default" translucent={false} />
-    <FlatList
-      data={currencies}
-      renderItem={({ item }) => <Text>{item}</Text>}
-      keyExtractor={(item) => item}
-    />
-  </View>
-)
+class CurrencyList extends Component {
+  handlePress = () => {
+    console.log('row press');
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="default" translucent={false} />
+        <FlatList
+          data={currencies}
+          renderItem={({ item }) =>
+            <ListItem text={item}
+              selected={item === TEMP_CURRENT_CURRENCY}
+              onPress={this.handlePress}
+            />
+          }
+          keyExtractor={(item) => item}
+          ItemSeparatorComponent={Separator}
+        />
+      </View>
+    )
+  }
+}
+
 
 CurrencyList.propTypes = {
 
